0e8060bb8294806474e4a471f7f445be157a7602
Jon Cheng <joncheng.dev@gmail.com>
2024-06-26 16:29:20 -0700

    add model validation for baby

--- EluneApi/Models/Baby.cs
+++ EluneApi/Models/Baby.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EluneApi.Models
 {
   public class Baby
   {
     public int BabyId { get; set; }
+    [Required]
+    [StringLength(2, 50, ErrorMessage = "Name must be between 2 and 50 characters long.")]
     public string Name { get; set; }
     public DateTime BirthDate { get; set; }
   }
