2d98709f3660bb6bae4217cab1e17062e2d3d0c1
Jon Cheng <joncheng.dev@gmail.com>
2024-06-30 13:46:56 -0700

    update error message for data annotations

---
+++ EluneApi/Migrations/20240630204519_UpdateAndTestDataAnnotations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EluneApi.Migrations
+{
+    public partial class UpdateAndTestDataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- EluneApi/Models/Baby.cs
+++ EluneApi/Models/Baby.cs
@@ -7,7 +7,7 @@ namespace EluneApi.Models
   {
     public int BabyId { get; set; }
     [Required]
-    [StringLength(50, MinimumLength = 5)]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters long.")]
     public string Name { get; set; }
     public DateTime BirthDate { get; set; }
   }
