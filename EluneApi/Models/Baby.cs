1f74ab77a496b01228b68718c4a1d9915d8a4329
Jon Cheng <joncheng.dev@gmail.com>
2024-07-01 00:17:21 -0700

    WIP: add sleep times to baby model

--- EluneApi/Models/Baby.cs
+++ EluneApi/Models/Baby.cs
@@ -10,5 +10,18 @@ namespace EluneApi.Models
     [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters long.")]
     public string Name { get; set; }
     public DateTime BirthDate { get; set; }
+
+    public ICollection<SleepTime> SleepTimes { get; set; }
+
+  }
+
+  public class SleepTime
+  {
+      public int SleepTimeId { get; set; }
+      public DateTime StartTime { get; set; }
+      public DateTime EndTime { get; set; }
+
+      public int BabyId { get; set; }
+      public Baby Baby { get; set; }
   }
 }
